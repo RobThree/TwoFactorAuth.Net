5c6d4b7d0977814e41f10316e32187b3a5eb8153
RobThree <rob@devcorner.nl>
2018-02-24 22:16:43 +0100

    * Update packages

--- TwoFactorAuth.Net.Demo/Scripts/_references.js
+++ TwoFactorAuth.Net.Demo/Scripts/_references.js
@@ -1,5 +1,5 @@
-﻿/// <reference path="modernizr-2.8.3.js" />
-/// <reference path="jquery-3.1.1.js" />
+﻿/// <reference path="jquery-3.3.1.js" />
+/// <reference path="modernizr-2.8.3.js" />
 /// <autosync enabled="true" />
 /// <reference path="bootstrap.min.js" />
 /// <reference path="jquery.validate.min.js" />
