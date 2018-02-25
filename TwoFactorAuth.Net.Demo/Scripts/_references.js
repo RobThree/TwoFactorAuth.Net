bfa24efc32e04910f70b9347322c3e66f8cd2100
RobThree <rob@devcorner.nl>
2018-02-25 02:00:59 +0100

    * Remove stale references

--- TwoFactorAuth.Net.Demo/Scripts/_references.js
+++ TwoFactorAuth.Net.Demo/Scripts/_references.js
@@ -4,7 +4,5 @@
 /// <reference path="bootstrap.min.js" />
 /// <reference path="jquery.validate.min.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
-/// <reference path="jquery-3.1.1.min.js" />
-/// <reference path="jquery-3.1.1.slim.min.js" />
 /// <reference path="respond.matchmedia.addlistener.min.js" />
 /// <reference path="respond.min.js" />
