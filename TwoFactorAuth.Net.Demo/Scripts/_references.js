df419288cce6985cf01d700c777783cd06b5b52d
RobThree <rob@devcorner.nl>
2020-06-02 01:34:40 +0200

    Updated packages. GetQrText() is now a public method. Fixed some project/solution weirdness. Upped version to 1.3.9

--- TwoFactorAuth.Net.Demo/Scripts/_references.js
+++ TwoFactorAuth.Net.Demo/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-3.3.1.js" />
+﻿/// <reference path="jquery-3.5.1.js" />
 /// <reference path="modernizr-2.8.3.js" />
 /// <autosync enabled="true" />
 /// <reference path="bootstrap.min.js" />
